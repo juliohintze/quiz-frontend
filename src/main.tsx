3ec63103516d4cf4c39de532a76f213f4d07b47a
Júlio Hintze <juliohintze@gmail.com>
2024-12-04 20:38:34 -0300

    New deps: react-router and eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
